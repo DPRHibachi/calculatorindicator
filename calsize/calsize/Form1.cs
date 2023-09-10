9a0a7f9c7537d23ea570155b666db90691880ddd
DPRHibachi <143536596+DPRHibachi@users.noreply.github.com>
2023-09-10 19:10:16 +0800

    Add files via upload

---
+++ calsize/calsize/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calsize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        progress prog = new progress();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num1 = double.Parse(textBox1.Text);
+                double num2 = double.Parse(textBox2.Text);
+                string operation = textBox3.Text.ToString();
+
+                double result = prog.operationprog(num1, num2, operation);
+                label1.Text = "" + result;
+
+                int resulta = Convert.ToInt32(label1.Text);
+                prog.total(resulta);
+
+                MessageBox.Show("" + resulta);
+
+            }
+            catch (FormatException)
+            {
+
+                MessageBox.Show("please input format");
+            }
+            catch (ArgumentException ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ calsize/calsize/progress.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calsize
+{
+    internal class progress
+    {
+
+        public void total (int code) 
+        {
+            if (code >= 1 && code <= 10)
+            {
+                MessageBox.Show(code + " " + "S");
+            }
+            else if (code >= 11 && code <= 15)
+            {
+                MessageBox.Show(code + " " + "M");
+            }
+            else if (code >= 16 && code <= 31)
+            {
+                MessageBox.Show(code + " " + "L");
+            }
+            else if (code >= 32 && code <= 100)
+            {
+                MessageBox.Show(code + " " + "XL");
+            }
+
+
+        }
+
+
+
+        public  static double add (double num1, double num2) 
+        {
+            return num1 + num2;
+        }
+
+        public static double minus(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double multi(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public double operationprog(double num1, double num2, string operation) 
+        {
+            switch(operation)
+                {
+                case "+":
+                    return add(num1, num2);
+
+                case "-":
+                    return minus(num1, num2);
+
+                case "*":
+                    return multi(num1, num2);   
+
+                case "/":
+                    return divide(num1, num2);
+
+                default:
+                    throw new ArgumentException("invalid operation");
+                
+                
+                
+                
+                }
+        
+        
+        }
+    }
+}
